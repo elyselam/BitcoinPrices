57e1932cca651c5b2bc334eb01582ad735e1e942
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2018-01-20 00:29:32 -0800

    Update Bitcoin_Price.py

--- Bitcoin_Price.py
+++ Bitcoin_Price.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,6 +8,7 @@ price = pd.read_csv('bitcoin_price.csv')
 
 trade_volume = pd.read_csv('trade_volume.csv')
 
+#split out the last part, which is the year
 def get_year(x):
     Date = x.split("/")
     year = Date[2]
@@ -16,10 +18,13 @@ price['year'] = price.Date.apply(get_year)
 price.head()
 
 
+#find the mean price in each year
+
 price_group = price["Price"].groupby(price["year"]).mean()
 
 price_df = price_group.reset_index()
 price_df 
+
 #convert to numeric
 price_df["year"] = pd.to_numeric(price_df["year"], errors='coerce')
 
@@ -28,11 +33,8 @@ price_df["Year"] = 2000 + price_df["year"]
 
 price_df.head()
 
-#delete year column
+# Cleaning Trade Volume DF
 
-price_df.drop(["year"], inplace=True)
-
-#CLEAN TRADE_VOLUME FILE
 trade_volume['year'] = trade_volume.Date.apply(get_year)
 trade_volume.head()
 
@@ -42,21 +44,38 @@ trade_volume_group = trade_volume["Volume"].groupby(price["year"]).mean()
 trade_volume_df = trade_volume_group.reset_index()
 trade_volume_df 
 
-
 #convert to numeric
 trade_volume_df["year"] = pd.to_numeric(trade_volume_df["year"], errors='coerce')
-
 
 #format Year 
 trade_volume_df["Year"] = 2000 + trade_volume_df["year"]
 
 trade_volume_df
 
-
 merged_df = pd.merge(price_df, trade_volume_df, on="Year")
 merged_df
 
-
 #drop year column
-merged_df.drop("year", inplace=True, axis=1)
+merged_df.drop("year_x", "year_y", inplace=True)
+
 merged_df.head()
+
+
+#print price chart
+merged_df.plot(x="Year", y="Price", kind = 'line', color = 'blue',label = 'Price',alpha = 0.5, grid = True)
+
+plt.xlabel('Years')              
+plt.ylabel('Price')
+plt.title('Price over Years')    
+plt.savefig("price.png")
+plt.show()
+
+
+#print Volume chart
+merged_df.plot(x="Year", y="Volume", kind = 'line', color = 'blue',label = 'Volume',alpha = 0.5, grid = True)
+
+plt.xlabel('Years')              
+plt.ylabel('Volume (Millions)')
+plt.title('Trading Volume over Years') 
+plt.savefig("volume.png")
+plt.show()
